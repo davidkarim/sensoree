976e3c4e876761722859bc78038254c20c4959ae
David Karim <davidkarim@gmail.com>
2016-05-18 17:59:43 -0400

    Partial seed table, new enum for sensor model

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -2,4 +2,9 @@ class Sensor < ActiveRecord::Base
   belongs_to :user
   has_many :events
   has_many :notifications
+
+  enum unit: [:Celsius, :Fahrenheit, :cm, :in, :feet, ]
+  enum kind: [:temperature, :ultrasound]
+  enum type_of_graph: [:bar_graph1, :bar_graph2, :on_off]
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user_list = [
+  ["asdf", "asdf@email.com","asdf"],
+  ["david", "dk@email.com","foobar"],
+  ["juan", "juan@email.com","foobar"],
+  ["jimbo", "jimbo@jim.com", "foobar"]
+]
+
+# sensor_list = [
+#   ["sensor1", 1, 1, ]
+# ]
+user_list.each do | username, email, password |
+  User.create(username: username, email: email, password: password)
+end
