034f1157be17438826f74315f9973bde1a18a8c6
David Karim <davidkarim@gmail.com>
2016-05-21 19:41:59 -0400

    Updates to events controller. Correction to HTML tags on new.html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    # binding.pry
     user_values = user_params.merge(api_key: generate_api)
     @user = User.new user_values
 
@@ -14,16 +15,6 @@ class UsersController < ApplicationController
 
   private
 
-  def generate_api
-    alpha = ('A'..'Z').to_a
-    alphanum = alpha + ('0'..'9').to_a
-    api_key = alpha.sample
-    19.times do
-      api_key += alphanum.sample
-    end
-    api_key
-  end
-  
   def user_params
     params.require(:user).
     permit(:username,
