95cad1a8e769b1d4f3c6f877f2c5af45515bbb79
David Karim <davidkarim@gmail.com>
2016-05-26 23:40:45 -0400

    Removed email test. Corrected email field on new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,26 +16,4 @@ class User < ActiveRecord::Base
     api_key
   end
 
-  def send_email
-    email_subject = "TEST"
-
-    message = <<MESSAGE_END
-From: Private Person <me@fromdomain.com>
-To: A Test User <dkarim@msn.com>
-MIME-Version: 1.0
-Content-type: text/html
-Subject: SMTP e-mail test
-
-This is an e-mail message to be sent in HTML format
-
-<b>This is HTML message.</b>
-<h1>This is headline.</h1>
-MESSAGE_END
-    Net::SMTP.start('localhost') do |smtp|
-    smtp.send_message message, 'me@fromdomain.com',
-                             'dkarim@msn.com'
-    end
-
-  end
-
 end
